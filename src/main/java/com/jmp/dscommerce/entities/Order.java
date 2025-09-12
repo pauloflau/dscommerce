251576ceb3d8913507f84eb21554286d955e0570
pauloflau <pauloflau@gmail.com>
2025-09-12 18:17:30 -0300

    refatorando a entidade category e product

--- src/main/java/com/jmp/dscommerce/entities/Order.java
+++ src/main/java/com/jmp/dscommerce/entities/Order.java
@@ -2,6 +2,7 @@ package com.jmp.dscommerce.entities;
 
 import java.time.Instant;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
@@ -88,5 +89,14 @@ public class Order {
 	public void setClient(User client) {
 		this.client = client;
 	}
+
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+
+	public List<Product> getProducts(){
+		return items.stream().map(x -> x.getProduct()).toList();
+	}
+	
 
 }
