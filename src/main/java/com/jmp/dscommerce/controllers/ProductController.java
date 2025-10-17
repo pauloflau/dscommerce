6a5c834f56c785e26fa31a40661ff62fc0d32aae
pauloflau <pauloflau@gmail.com>
2025-10-17 19:06:50 -0300

    adicionado controle por perfil e rota

--- src/main/java/com/jmp/dscommerce/controllers/ProductController.java
+++ src/main/java/com/jmp/dscommerce/controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,18 +31,21 @@ public class ProductController {
 	@Autowired
 	ProductService service;
 	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@DeleteMapping(value="/{id}")
 	public ResponseEntity<ProductDto> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();		
 	}
 	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@PutMapping(value="/{id}")
 	public ResponseEntity<ProductDto> update(@PathVariable Long id, @Valid @RequestBody ProductDto dto) {
 		dto = service.update(id, dto);
 		return ResponseEntity.ok(dto);		
 	}
 	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@PostMapping
 	public ResponseEntity<ProductDto> insert(@Valid @RequestBody ProductDto dto) {
 		dto = service.insert(dto);
